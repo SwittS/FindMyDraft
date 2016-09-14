ed23a2a227c7126520fcc742c54820309fa631ad
SwittS <switt.srikulacheep@gmail.com>
2016-09-14 15:35:26 -0700

    remove jumbotron bg for visual purposes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,7 @@ $(document).on('turbolinks:load', function() {
  $(".button-collapse").sideNav();
  $(".dropdown-button").dropdown();
  $('select').material_select();
+ $('.slider').slider({full_width: true});
  Waves.displayEffect();
 
  Materialize.updateTextFields();
