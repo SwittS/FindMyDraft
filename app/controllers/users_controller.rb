359aaa353ab48b0cd0c5c0db39df80bc121e049a
SwittS <switt.srikulacheep@gmail.com>
2016-09-13 20:38:52 -0700

    structure layout for venues index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-
-    @venue = Venue.find_by_user_id(@user.id)
-
-    @booking = Booking.find_by_venue_id(@venue.id)
   end
 end
