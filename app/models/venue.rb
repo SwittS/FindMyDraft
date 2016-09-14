87b0f5855bbf4dbf8632772e58d39470c6eaf82d
SwittS <switt.srikulacheep@gmail.com>
2016-09-14 01:29:44 -0700

    basic validations for models

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :address, :wifi, :accomodate, presence: true
+  validates :venue_name, length: {maximum: 64}
 end
